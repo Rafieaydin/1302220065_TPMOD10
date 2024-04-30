4d0280a997ee62ca5dc1d002282707c0363222d6
Rafieaydin <rafie.aydin1@gmail.com>
2024-04-30 13:39:54 +0700

    add crud mahasiswa

---
+++ TPMOD10/Controllers/MahasiswaController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TPMOD10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> mhs = new List<Mahasiswa> {
+            new Mahasiswa { id  =  1, Nama = "",Nim="",Year=0 },
+            new Mahasiswa { id = 2, Nama = "", Nim = "", Year = 0 },
+            new Mahasiswa { id = 3, Nama = "", Nim = "", Year = 0 }
+        };
+        
+        // GET: api/<MahasiswaController>
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mhs;
+        }
+
+        // GET api/<MahasiswaController>/5
+        [HttpGet("{id}")]
+        public Mahasiswa GetById(int id)
+        {
+            for (int i = 0; i < mhs.Count; i++)
+            {
+                if (mhs[i].id == id) 
+                {
+                    return mhs[i];
+                }
+            }
+            return null;
+        }
+
+        // POST api/<MahasiswaController>
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+           mhs.Add(value);
+        }
+
+        // PUT api/<MahasiswaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Mahasiswa value)
+        {
+            int idx = 0;
+            for (int i = 0; i < mhs.Count; i++)
+            {
+                if (mhs[i].id == id)
+                {
+                   idx = i;
+                }
+            }
+            mhs[idx] = value;
+            return ;
+
+        }
+
+        // DELETE api/<MahasiswaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Mahasiswa idx = null;
+            for (int i = 0; i < mhs.Count; i++)
+            {
+                if (mhs[i].id == id)
+                {
+                    idx = mhs[i];
+                }
+            }
+            mhs.Remove(idx);
+        }
+    }
+}
---
+++ TPMOD10/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace TPMOD10
+{
+    public class Mahasiswa
+    {
+        public int id {  get; set; }    
+        public String Nama { set; get; }
+        public String Nim { set; get; }
+        public List<String> Course { set; get; }
+        public int Year { set; get; }
+
+
+    }
+}
